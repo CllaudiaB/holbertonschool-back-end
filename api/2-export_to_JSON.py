8e965568a5de8075827adf8a719f25a9f4637bb7
CllaudiaB <buraanna27@gmail.com>
2023-04-06 05:49:51 -0700

    fixed error

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -8,29 +8,29 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    todos_data = todos.json()
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
-    users_data = users.json()
+    response_user = requests.get("https://jsonplaceholder.typicode.com/users")
+    response_todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    user = response_user.json()
+    todos = response_todos.json()
 
-    for data in users_data:
-        if int(argv[1]) == data.get("id"):
-            user_id = data.get("id")
-            username = data.get("username")
+    for item in user:
+        if int(sys.argv[1]) == item.get('id'):
+            n = item.get('id')
+            user_name = item.get('username')
 
-    list_dict = []
+    li = []
 
-    for data in todos_data:
-        dict1 = {}
-        if int(argv[1]) == data.get("userId"):
-            dict1["task"] = data.get("title")
-            dict1["completed"] = data.get("completed")
-            dict1["username"] = username
-            list_dict.append(dict1)
+    for item in todos:
+        _dict = {}
+        if int(sys.argv[1]) == item.get('userId'):
+            _dict['task'] = item.get('title')
+            _dict['completed'] = item.get('completed')
+            _dict1['username'] = user_name
+            li.append(_dict)
 
-    dict2 = {}
-    dict2[user_id] = list_dict
+    _dict1 = {}
+    _dict1[n] = li
 
-    json_dict = json.dumps(dict2)
+    json_dict = json.dumps(_dict1)
     with open(str(argv[1]) + ".json", "w") as file:
         file.write(json_dict)
