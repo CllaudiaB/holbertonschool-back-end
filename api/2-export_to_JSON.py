2abc033e7c9574b64009da7b2b142e6d1bd0c2ed
CllaudiaB <buraanna27@gmail.com>
2023-04-06 05:34:00 -0700

    fixed error

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
 """
-Using what you did in the task #0, extend your Python script to export data
-in the JSON format
+    script to export data in the JSON format
 """
 import json
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    todos_data = todos.json()
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users_data = users.json()
 
-    response_user = requests.get('https://jsonplaceholder.typicode.com/users')
-    response_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    user = response_user.json()
-    todos = response_todos.json()
+    for data in users_data:
+        if int(argv[1]) == data.get("id"):
+            user_id = data.get("id")
+            username = data.get("username")
 
-    for item in user:
-        if int(sys.argv[1]) == item.get('id'):
-            n = item.get('id')
-            user_name = item.get('username')
+    list_dict = []
 
-    li = []
-    for item in todos:
-        _dict = {}
-        if int(sys.argv[1]) == item.get('userId'):
-            title = item.get('title')
-            task = item.get('completed')
-            _dict['title'] = title
-            _dict['completed'] = task
-            _dict['username'] = user_name
-            li.append(_dict)
+    for data in todos_data:
+        dict1 = {}
+        if int(argv[1]) == data.get("userId"):
+            dict1["task"] = data.get("title")
+            dict1["completed"] = data.get("completed")
+            dict1["username"] = username
+            list_dict.append(dict1)
 
-    _dict1 = {}
-    _dict1[n] = li
-    _json = json.dumps(_dict1)
-    with open(str(sys.argv[1]) + ".json", "w") as file:
-        file.write(_json)
+    dict2 = {}
+    dict2[user_id] = list_dict
+
+    json_dict = json.dumps(dict2)
+    with open(str(argv[1]) + ".json", "w") as file:
+        file.write(json_dict)
